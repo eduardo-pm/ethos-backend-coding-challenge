319bc774dda466cc64293e9d367c493b652f1048
Eduardo Perez Mondragon <eduardo.perezm33@gmail.com>
2026-03-03 09:12:33 -0600

    chore: initialize Spring Boot 3.5.11 project with base dependencies

---
+++ src/main/java/com/ethos/backoffice/BackofficeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ethos.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApiApplication.class, args);
+	}
+
+}
